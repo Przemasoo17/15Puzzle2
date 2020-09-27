281c006e642b6b3cb584f9343fcb347e7af538d0
Przemyslaw Dobrowolski <dobrowolski.przemyslaw.1991@gmail.com>
2020-09-27 19:10:36 +0200

    fisrt commit

---
+++ src/pkg15puzzle/Main.java
@@ -0,0 +1,16 @@
+
+package pkg15puzzle;
+
+public class Main 
+{
+    // Size of our Game of Fifteen instance
+    private int size; 
+    // Number of tiles
+    private int nbTiles;
+    // Grid UI Dimension
+    private int dimension;
+    // Foreground Color
+    private static final Color FOREGROUND_COLOR = ne Color(239, 83, 80); // we use arbitrary color
+    
+    
+}
